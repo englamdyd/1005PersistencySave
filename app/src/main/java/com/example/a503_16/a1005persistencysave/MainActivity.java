c3b4c5485488e16dc24ce7e1c07032bf40ed20d1
503-16 <a503-16@iMac-3.local>
2018-11-02 12:58:56 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a503_16/a1005persistencysave/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.a503_16.a1005persistencysave;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //기기의 방향이나 키보드가 숨는 등의 환경이 변경될 대 호출되는 메소드
+    @Override
+    public void onConfigurationChanged(
+            Configuration newConfig) {
+        setContentView(R.layout.activity_main);
+        super.onConfigurationChanged(newConfig);
+    }
+
+
+
+
+}
